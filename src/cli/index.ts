e854b56e311ca25c4cf852f39bbee72caebe6d1c
Ugo ROSERAT <roserat.ugo@gmail.com>
2025-12-27 14:17:36 +0100

    chore: bump version to 0.1.4

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -11,7 +11,7 @@ import { removeCommand } from "./commands/remove";
 const main = defineCommand({
   meta: {
     name: "orpheus",
-    version: "0.1.3",
+    version: "0.1.4",
     description: "Now Playing overlay for OBS - Works with any music service",
   },
   subCommands: {
