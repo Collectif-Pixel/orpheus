25c94b9cec733c23bcd60e2d2fa2a7aa932ed05f
Ugo ROSERAT <roserat.ugo@gmail.com>
2025-12-28 14:09:42 +0100

    🔖 Bump version to 0.2.1

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
 export const SSE_KEEP_ALIVE_MS = 15000;
 export const DAEMON_START_DELAY_MS = 800;
 export const DEFAULT_PORT = 4242;
-export const VERSION = "0.2.0";
+export const VERSION = "0.2.1";
